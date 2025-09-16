0c010f93c1fd53a9511f16e4e8a940278a1d6c97
AlexKlos <klos@list.ru>
2025-09-16 18:30:27 +0700

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from environs import env
+from google.cloud import dialogflow
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(
@@ -14,24 +15,43 @@ def start(update, context):
     update.message.reply_text('Здравствуйте!')
 
 
-# def help_command(update, context):
-#     update.message.reply_text('Help!')
-
-
 def echo(update, context):
     update.message.reply_text(update.message.text)
 
 
+def get_dialog_flow_response(project_id, session_id, text):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code='Russian — ru')
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result.fulfillment_text
+
+
+def reply_via_dialogflow(update, context):
+    user_text = (update.message.text or "").strip()
+    project_id = context.bot_data.get("DIALOG_FLOW_PROJECT_ID")
+    session_id = update.effective_user.id
+
+    answer = get_dialog_flow_response(project_id, session_id, user_text)
+    update.message.reply_text(answer)
+
+
 def main():
     env.read_env()
-    TG_BOT_TOKEN = env.str('TG_BOT_TOKEN')
-    updater = Updater(TG_BOT_TOKEN, use_context=True)
 
+    TG_BOT_TOKEN = env.str('TG_BOT_TOKEN')
+    DIALOG_FLOW_PROJECT_ID = env.str('DIALOG_FLOW_PROJECT_ID')
+
+    updater = Updater(TG_BOT_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
+    dispatcher.bot_data["DIALOG_FLOW_PROJECT_ID"] = DIALOG_FLOW_PROJECT_ID
     dispatcher.add_handler(CommandHandler("start", start))
-    # dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_via_dialogflow))
 
     updater.start_polling()
     updater.idle()
